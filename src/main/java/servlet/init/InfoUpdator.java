c35af73338fe9d52e8e63e72f7ea775ac6680e57
jt <jt2594838@163.com>
2017-07-19 21:55:39 +0800

    add init servlet to start colloctor thread

---
+++ src/main/java/servlet/init/InfoUpdator.java
@@ -0,0 +1,14 @@
+package servlet.init;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+public class InfoUpdator extends HttpServlet {
+
+	@Override
+	public void init() throws ServletException {
+		// TODO create a thread to update proj info
+		System.out.println("no hello please");
+	}
+
+}
